ebd0ee71a3e5d9ed942b2e1b6a5bfbc0563453c4
AlbeTorres <albertocorreoficial@gmail.com>
2022-12-06 10:19:59 -0500

    mejorado responsive de trabajadores

--- AsistoApp/src/components/usuario/TrabajadorCard.jsx
+++ AsistoApp/src/components/usuario/TrabajadorCard.jsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import worker from '../../assets/noperfil.jpg'
+import '../../index.css'
 
 const TrabajadorCard = () => {
   return (
-    <div className=' grid grid-rows-2 md:grid-cols-4 md:grid-rows-1 h-fit py-4 gap-4 place-items-center border-gray-200 border-b   '>
-        
-        <div>
+    <div className=' md:grid usuario-card h-fit py-4 gap-4   border-gray-200 border-b   '>
+        <figure className='h-24 w-24 rounded-md '>
+      <img src={worker} alt="" className='h-full w-full object-cover rounded-md' />
+    </figure>
+        <div  className='flex flex-col items-start justify-center pl-2'>
         <p className='text-gray-500'>
             Alberto Torres
         </p>
-        </div>
-        <div>
-        <p className='text-gray-500'>
-            Departamento
+        <p className= 'md:hidden text-gray-500'>
+        9900330044
         </p>
         </div>
-        <div>
+        <div   className=' hidden md:flex flex-col items-start justify-center pl-2'>
         <p className='text-gray-500'>
-            Cargo
+        9900330044
         </p>
         </div>
-        <div className='grid place-items-center '>
+        <div  className='hidden md:flex flex-col items-start justify-center pl-2'>
+        <p className='text-gray-500'>
+        Cargo
+        </p>
+        </div>
+        <div className='flex flex-col items-center justify-center '>
             <label htmlFor="my-modal" className="btn modal-button bg-gray-800 text-white">Editar</label>
             
             
